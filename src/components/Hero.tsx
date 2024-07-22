2d3404dfcc7837b818d400b068e3c3b918e472bb
Justin Fish <justinanthonyfish@gmail.com>
2024-07-22 20:45:25 +0100

    Commented out Modal

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,21 @@
-import { useRef } from 'react';
+// import { useRef } from 'react';
 import { Button } from "./ui/button";
-import Modal from './Modal.jsx'
 
 export const Hero = () => {
-  const modal = useRef();
+  // const modal = useRef();
 
-  function handleOpenWaitingList() {
-    modal.current.open();
-  }
+  // function handleOpenWaitingList() {
+  //   modal.current.open();
+  // }
 
   return (
 <>
-<Modal ref={modal} buttonCaption="Close" >
+{/* <Modal ref={modal} buttonCaption="Close" >
         {" "}
         <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
         <p className="text-stone-400 mb-4">Forgot to enter info</p>
         <p className="text-stone-400 mb-4">enter the information</p>
-      </Modal>{" "}
+      </Modal>{" "} */}
 
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
@@ -34,7 +33,7 @@ export const Hero = () => {
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button onClick={handleOpenWaitingList} className="w-full md:w-1/3">Join Waiting List</Button>
+          <Button className="w-full md:w-1/3">Join Waiting List</Button>
         </div>
       </div>
 
