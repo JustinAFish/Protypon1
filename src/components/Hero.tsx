0bc269f29f584f5d543fe08c46b3d9178326443b
Justin Fish <justinanthonyfish@gmail.com>
2024-07-22 20:40:03 +0100

    Starting Modal

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,23 @@
+import { useRef } from 'react';
 import { Button } from "./ui/button";
+import Modal from './Modal.jsx'
 
 export const Hero = () => {
+  const modal = useRef();
+
+  function handleOpenWaitingList() {
+    modal.current.open();
+  }
+
   return (
+<>
+<Modal ref={modal} buttonCaption="Close" >
+        {" "}
+        <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
+        <p className="text-stone-400 mb-4">Forgot to enter info</p>
+        <p className="text-stone-400 mb-4">enter the information</p>
+      </Modal>{" "}
+
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl font-bold  max-w-1">
@@ -18,12 +34,13 @@ export const Hero = () => {
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Join Waiting List</Button>
+          <Button onClick={handleOpenWaitingList} className="w-full md:w-1/3">Join Waiting List</Button>
         </div>
       </div>
 
       {/* Shadow effect */}
       <div className="shadow"></div>
     </section>
+    </>
   );
 };
